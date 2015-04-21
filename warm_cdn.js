e8736122d554aff76df9f79d31d1909b39c76857
Zane <zane.williamson@gmail.com>
2015-04-21 00:01:41 -0700

    adding for loop to function

--- warm_cdn.js
+++ warm_cdn.js
@@ -1,13 +1,15 @@
 var http = require('http');
 
 exports.handler = function(event, context) {
-  console.log('start request to ' + event.url)
-  http.get(event.url, function(res) {
-    console.log("Got response: " + res.statusCode);
-  }).on('error', function(e) {
-    console.log("Got error: " + e.message);
-  });
+  for (var key in event ) {
+        console.log('start request to ' + event[key]);
+        http.get(event[key], function(res) {
+          console.log("Got response: " + res.statusCode);
+        }).on('error', function(e) {
+          console.log("Got error: " + e.message);
+        });
 
-  console.log('end request to ' + event.url)
-  context.done(null);
-}
+        console.log('end request to ' + event[key]);
+   }
+   context.done();
+};
