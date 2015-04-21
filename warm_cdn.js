806bb922743962b0d9da140d4ad52e4306b8b33b
Zane <zane.williamson@gmail.com>
2015-04-20 23:32:48 -0700

    works for one url, but need to fix to handle each

--- warm_cdn.js
+++ warm_cdn.js
@@ -1,14 +1,13 @@
 var http = require('http');
 
-exports.getURLs = function (event, element) {
-    http.get(element, function(res) {
-      console.log("http get... " + element);
-      console.log("Got response: " + res.statusCode );
-      res.on('data', function(chunk) {
-        //console.log('BODY: '  + chunk);
-      });
-     }).on('error', function(e) {
-      console.log("Got error: " + e.message);
+exports.handler = function(event, context) {
+  console.log('start request to ' + event.url)
+  http.get(event.url, function(res) {
+    console.log("Got response: " + res.statusCode);
+  }).on('error', function(e) {
+    console.log("Got error: " + e.message);
   });
-  element.succeed("Completed warm of URLs.");
+
+  console.log('end request to ' + event.url)
+  context.done(null);
 }
