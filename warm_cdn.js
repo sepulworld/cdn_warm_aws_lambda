82eca1cc401da9a6f6921c8f38f2a93f91b27b53
Zane Williamson <sepulworld@gmail.com>
2015-05-19 22:07:19 -0700

    Push status codes into responses array

--- warm_cdn.js
+++ warm_cdn.js
@@ -1,19 +1,21 @@
 var http = require('http');
 
 exports.handler = function(event, context) {
-var responses = {},
-count = 0;
+var responses = [];
+var count = 0;
 
   for (var key in event ) {
         console.log('start request to ' + event[key]);
         http.get(event[key], function(res) {
           console.log("Got response: " + res.statusCode);
+          responses.push(res.statusCode);
           count++;
           if (count == Object.keys(event).length) { 
             callback(responses, context);
           }
         }).on('error', function(e) {
           console.log("Got error: " + e.message);
+          responses.push(res.statusCode);
           count++;
           if (count == Object.keys(event).length) { 
             callback(responses, context);
@@ -25,6 +27,7 @@ count = 0;
 }
   
 function callback(responses, context) {
-  context.done()
+    context.succeed(responses)
+    context.done();
   }
 }
