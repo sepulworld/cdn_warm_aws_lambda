c1a46c89f4cddbb8a1b7660458327ada430b09c6
Zane Williamson <sepulworld@gmail.com>
2015-05-19 20:43:48 -0700

    Update to use callback

--- warm_cdn.js
+++ warm_cdn.js
@@ -1,15 +1,28 @@
 var http = require('http');
 
 exports.handler = function(event, context) {
+var responses = {},
+count = 0;
+
   for (var key in event ) {
         console.log('start request to ' + event[key]);
         http.get(event[key], function(res) {
           console.log("Got response: " + res.statusCode);
+          count++;
+          if (count == Object.keys(event).length) { 
+            callback(responses, context);
+          }
+          console.log(count)
         }).on('error', function(e) {
           console.log("Got error: " + e.message);
+          console.log(count)
         });
 
         console.log('end request to ' + event[key]);
-   }
-   context.done();
-};
+    
+}
+  
+function callback(responses, context) {
+  context.done()
+  }
+}
